a7658247d5ff1ac936fe16242f319bb05bf87163
erdoctors2015 <87606049+erdoctors2015@users.noreply.github.com>
2025-07-30 11:42:21 +0300

    Add files via upload

--- cssandjs/exam/review-3a5d3d1dd3170c0ecf989794e8b4fce501cb95f8aa760e28e10e885ec8c20b45.js
+++ cssandjs/exam/review-3a5d3d1dd3170c0ecf989794e8b4fce501cb95f8aa760e28e10e885ec8c20b45.js
@@ -1,1 +1,49 @@
-NormalLabValues={close_values_popup_on_click:function(){$("#close_values_popup").on("click",close_values_popup)},open_values_popup_on_click:function(){$("#open_values_popup").on("click",lab_values_popup)}},Formatting={init:function(){this.bindEvents()},bindEvents:function(){$("#highlight").on("click",highlight_text),$("#strikeout").on("click",strikeout_text),$("#erase").on("click",erase_text),$("#flag_question").on("click",flag_this_question),$("#normal_lab_values").on("click",lab_values_popup)}},Review={init:function(){this.bindEvents()},bindEvents:function(){NormalLabValues.open_values_popup_on_click(),NormalLabValues.close_values_popup_on_click()}},$(function(){Review.init(),Formatting.init()});
+function highlight_text() {
+  const selection = window.getSelection().toString();
+  if (selection.length > 0) {
+    const span = document.createElement("span");
+    span.style.backgroundColor = "#ffff66"; // لون التمييز الأصفر
+    span.textContent = selection;
+
+    const range = window.getSelection().getRangeAt(0);
+    range.deleteContents();
+    range.insertNode(span);
+  }
+}
+
+const NormalLabValues = {
+  close_values_popup_on_click: function () {
+    $("#close_values_popup").on("click", close_values_popup);
+  },
+  open_values_popup_on_click: function () {
+    $("#open_values_popup").on("click", lab_values_popup);
+  }
+};
+
+const Formatting = {
+  init: function () {
+    this.bindEvents();
+  },
+  bindEvents: function () {
+    $("#highlight").on("click", highlight_text);
+    $("#strikeout").on("click", strikeout_text);
+    $("#erase").on("click", erase_text);
+    $("#flag_question").on("click", flag_this_question);
+    $("#normal_lab_values").on("click", lab_values_popup);
+  }
+};
+
+const Review = {
+  init: function () {
+    this.bindEvents();
+  },
+  bindEvents: function () {
+    NormalLabValues.open_values_popup_on_click();
+    NormalLabValues.close_values_popup_on_click();
+  }
+};
+
+$(function () {
+  Review.init();
+  Formatting.init();
+});
